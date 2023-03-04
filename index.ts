10746319b95579dd6c6cb9ddfd08796eb957e57c
jmKeenan <john.keenan@gmail.com>
2023-03-04 17:05:14 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import Holidays from 'date-holidays';
 
-import { DateTime } from 'luxon';
-
 const howManyHours = (
   futureDate: Date = new Date(),
   past: Date = pastDate(6)
@@ -26,49 +24,35 @@ const howManyHours = (
   );
 };
 
-const getHolidaysOldestFirst = (holidays, y1: number, y2: number) =>
-  y1 === y2
-    ? holidays.concat(new Holidays('US').getHolidays(y1))
-    : 
-        getHolidaysOldestFirst(
-          holidays.concat(new Holidays('US').getHolidays(y2++)),
-          y1,
-          y2,
-          
-        
-      );
+const getPublicHolidays = (year: number) =>
+  new Holidays('US')
+    .getHolidays(year)
+    .filter(
+      ({ type, start }) =>
+        type === 'public' && !(start.getDay() === 0 || start.getDay() === 6)
+    );
 
-/*
-      const getHolidaysOldestFirst = (holidays, y1: number, y2: number) =>
+const getHolidaysOldestFirst = (holidays: any[], y1: number, y2: number) =>
   y1 === y2
-    ? holidays.concat(y1)
-    : getHolidaysOldestFirst(
-          holidays.concat(y2++),
-          y1,
-          y2,
-          
-          
-        
-      );
-*/
-let rec = n => n == 1 ? n : n + rec(n - 1);
-console.log(rec(10))
+    ? holidays.concat(getPublicHolidays(y1))
+    : getHolidaysOldestFirst(holidays.concat(getPublicHolidays(y2++)), y1, y2);
 
 const getMultiYearHolidays = (y1: number, y2: number) =>
   y1 > y2
     ? getHolidaysOldestFirst([], y1, y2)
     : getHolidaysOldestFirst([], y2, y1);
 
-console.log(getMultiYearHolidays(2014, 2012).filter(v=>v.name === 'Easter Sunday'))
+console.log(getMultiYearHolidays(2014, 2012));
 /**
  * Get's the hours of holidays between two dates (so if july 4 is betweeen, return 24, if july 4 and labor date, return 48)
- * @param {Date} futureDate
- * @param {Date} past
+ * @param {Date} d1
+ * @param {Date} d2
  * @return {number} hours of holidays between two dates
  */
-const holidayHours_ = (d1: Date = pastDate(-120), d2: Date = new Date()) => {
-  //onst v = new Holidays('US')
-
+const holidayHours_ = (
+  d1: Date = pastDate(-120),
+  d2: Date = new Date()
+): number => {
   const holidays =
     d1.getFullYear() === d2.getFullYear()
       ? new Holidays('US').getHolidays(d1.getFullYear())
@@ -86,9 +70,8 @@ const holidayHours_ = (d1: Date = pastDate(-120), d2: Date = new Date()) => {
  * Utility to create past dates
  * @param {number} daysAgo
  * @return {Date}
- *
  */
-const pastDate = (daysAgo = 10) =>
+const pastDate = (daysAgo: number = 10): Date =>
   ((pastDate) => new Date(pastDate.setDate(pastDate.getDate() - daysAgo)))(
     new Date()
   );
